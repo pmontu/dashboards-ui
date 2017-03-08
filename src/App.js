12fe6f95f8a19383d1bcd34985812d991a15a64d
pmontu <pmontu@gmail.com>
2017-03-08 21:53:52 +0530

    use axios post to oauth token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button, Grid, Row, Col, Form, FormGroup, ControlLabel, FormControl, PageHeader} from 'react-bootstrap';
+import axios from 'axios'
+import qs from 'qs'
 
+
+var api = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  auth: {
+    username: 'IZabfQXlLPhMIvdRIStoQ26DMZudAdUk65bs3iui',
+    password: 'SULQnwWmNMHE7wp6CnZulb4kSfb4QyFfKR5c5S3Euh5wbMbaLdde3eYZcF1zdyJ8WZPGHL3gsJuQ66xk7iVKFXvMVyYpfn7RihQenWMzziufbTmZB2O9Wmk6yEjegfg1'
+  },
+});
+
+const data = {
+  login: (username, password) => {
+
+    const params = qs.stringify({
+      'username': username,
+      'password': password,
+      'grant_type': "password"
+    })
+
+    console.log(params)
+
+    api.post('/o/token/', params)
+      .then(function (response) {
+        console.log(response);
+        api.defaults.headers.common['Authorization'] = response.data.access_token;
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data);
+        }
+        else {
+          console.log(error.message);
+        }
+      });
+  }
+}
 
 class LoginForm extends Component{
   constructor(props){
@@ -10,7 +47,8 @@ class LoginForm extends Component{
   }
   login(event){
     event.preventDefault();
-    console.log(this.username.value, this.password.value)
+    console.log(this.username.value, this.password.value);
+    data.login(this.username.value, this.password.value);
   }
   render(){
     return (
@@ -46,9 +84,7 @@ class App extends Component {
     return (
       <Grid>
 
-        <Row className="show-grid"><Col xs={12}>{'.'}</Col></Row>
-
-        <Row className="show-grid">
+        <Row className="show-grid top-buffer">
           <Col xs={5}></Col>
           <Col xs={7}>
             <LoginForm />
